8cb731eb36393ed98da233f0a596f71b9e8525db
Clint Banzhaf <clint.banzhaf@gmx.net>
2015-06-17 19:14:47 +0200

    updated general.hlsl

--- shader/general.hlsl
+++ shader/general.hlsl
@@ -148,7 +148,7 @@ float4 ambient_ps(
 		ambient.b * texcol.b);
 	
 	// combine ambientlight and directionallight weightened
-	float3 sum = (0.3 * dir) + (0.7 * ambi);
+	float3 sum = (0.25 * dir) + (0.75 * ambi);
 	
 	// output pixel
 	return float4(
